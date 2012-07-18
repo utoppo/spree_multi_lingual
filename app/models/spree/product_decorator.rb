f0b9e7597bb09e01ab80e034320bb4222b4be570
42GB <georg.bitzer@googlemail.com>
2012-07-18 15:54:06 +0200

    added new field :short_description to translateable fields

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,3 +1,3 @@
 Spree::Product.class_eval do
-  translates :name, :description, :meta_description, :meta_keywords, :permalink
+  translates :name, :description, :meta_description, :meta_keywords, :permalink, :short_description
 end
